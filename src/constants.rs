65c9e8ffc9d6f74b668e94de694ef61c16820a70
Yumshot <tdsousa1993@gmail.com>
2024-09-26 14:16:21 -0700

    feat(constants): Adjusted temperature, repeat penalty and top-k values for improved model performance

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 pub const SYSTEM_PROMPT: &str =
-    "generate a commit message from our changes, DO NOT FORMAT YOUR COMMIT MESSAGE AND ONLY RESPOND WITH THE COMMIT MESSAGE REMOVE ```plaintext``` FROM YOUR RESPONSEuse Conventional Commits to describe changes and their impact, aligning with Semantic Versioning: fix: for bug fixes (PATCH), feat: for new features (MINOR), BREAKING CHANGE: for major API changes (MAJOR) indicated by ! or a BREAKING CHANGE: footer; other types like build:, chore:, docs:, style:, refactor:, perf:, test: provide context without affecting versioning unless marked as breaking; use feat!: or BREAKING CHANGE: to signal major changes; scopes (e.g., feat(api):) add context; commit types and scopes help maintainers and systems understand change impact";
+    "generate a commit message from our changes with a maximum length of 255 characters, DO NOT FORMAT YOUR COMMIT MESSAGE AND ONLY RESPOND WITH THE COMMIT MESSAGE REMOVE ```plaintext``` FROM YOUR RESPONSEuse Conventional Commits to describe changes and their impact, aligning with Semantic Versioning: fix: for bug fixes (PATCH), feat: for new features (MINOR), BREAKING CHANGE: for major API changes (MAJOR) indicated by ! or a BREAKING CHANGE: footer; other types like build:, chore:, docs:, style:, refactor:, perf:, test: provide context without affecting versioning unless marked as breaking; use feat!: or BREAKING CHANGE: to signal major changes; scopes (e.g., feat(api):) add context; commit types and scopes help maintainers and systems understand change impact";
 pub const MODEL: &str = "deepseek-coder-v2";
 pub const LOCALHOST: &str = "http://localhost";
 pub const LLM_PORT: u16 = 11434;
@@ -16,7 +16,7 @@ What is the nature of these changes?
 - (style): For changes that do not affect code functionality, such as formatting, spacing, or style adjustments.
 - (chore): For changes related to build processes, dependencies, or maintenance tasks. 
 Type: "#;
-pub const TEMPERATURE: f32 = 0.2;
-pub const REPEAT_PENALTY: f32 = 1.5;
-pub const TOP_K: u32 = 25;
+pub const TEMPERATURE: f32 = 0.1;
+pub const REPEAT_PENALTY: f32 = 1.2;
+pub const TOP_K: u32 = 35;
 pub const TOP_P: f32 = 0.25;
